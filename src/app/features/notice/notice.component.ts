df93aa8301e6a05c7e920eaf940713ac2f23e293
somnathdhar97 <somnathdhar2021@gmail.com>
2023-12-03 17:26:54 +0530

    notice is done.

--- src/app/features/notice/notice.component.ts
+++ src/app/features/notice/notice.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LayoutService } from 'src/app/core/layout/service/app.layout.service';
+import { ApiService } from 'src/app/core/services/api.service';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { ValidationService } from 'src/app/core/services/validation.service';
 
 @Component({
   selector: 'app-notice',
   templateUrl: './notice.component.html',
   styleUrls: ['./notice.component.scss']
 })
-export class NoticeComponent {
+export class NoticeComponent implements OnInit {
+  noticeForm: FormGroup;
+  cities: any[] = [];
 
+  constructor(public layoutService: LayoutService, public fb: FormBuilder, private vs: ValidationService, private apiService: ApiService, private authService: AuthService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.noticeForm = this.fb.group({
+      noticeHeading: this.vs.validation('Required', 0, 100, 100),
+      noticeMessage: this.vs.validation('Required', 0, 100, 100),
+      remarks: this.vs.validation('Required', 0, 100, 100),
+    });
+  }
+
+  get errorControl() {
+    return this.noticeForm.controls;
+  }
+
+  create() {
+    if (this.noticeForm.valid) {
+      console.log(this.noticeForm.value);
+      // this.apiService.register(this.noticeForm.value).subscribe(resp => {
+      //   if (resp.apiStatus == 1) {
+      //     alert("Created successfully..!")
+      //     this.noticeForm.reset();
+      //     this.router.navigate(['/dashboard'])
+      //   } else {
+      //     this.noticeForm.reset();
+      //     this.noticeForm.markAllAsTouched();
+      //   }
+      // });
+      // this.router.navigate(['/dashboard'])
+    } else {
+      this.noticeForm.markAllAsTouched();
+      alert("Please fill all the fields carefully..!");
+    }
+  }
 }
--- src/app/features/notice/notice.module.ts
+++ src/app/features/notice/notice.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PasswordModule } from 'primeng/password';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
+import { CustomMatErrorModule } from 'src/app/shared/helper/custom-mat-error/custom-mat-error.module';
 import { NoticeRoutingModule } from './notice-routing.module';
 import { NoticeComponent } from './notice.component';
 
@@ -11,7 +17,15 @@ import { NoticeComponent } from './notice.component';
   ],
   imports: [
     CommonModule,
-    NoticeRoutingModule
+    NoticeRoutingModule,
+    ButtonModule,
+    CheckboxModule,
+    InputTextModule,
+    FormsModule,
+    PasswordModule,
+    ReactiveFormsModule,
+    CustomMatErrorModule,
+    DropdownModule
   ]
 })
 export class NoticeModule { }
