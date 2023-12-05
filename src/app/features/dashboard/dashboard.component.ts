f0ba3c51f71fcd41e47fe6bccec4d4982290fa44
manwar <itsmanwar@gmail.com>
2023-12-06 02:39:15 +0530

    notification api added

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { IStatusWiseClient } from 'src/app/core/models/IClient';
 import { IStatusWiseInvesment } from 'src/app/core/models/Iinvesment';
+import { INotification } from 'src/app/core/models/notification';
 import { ClientService } from 'src/app/core/services/client.service';
 import { InvesmentService } from 'src/app/core/services/invesment.service';
+import { NotificationService } from 'src/app/core/services/notification.service';
 import { ToastService } from 'src/app/core/services/toast.service';
 
 @Component({
@@ -11,15 +13,17 @@ import { ToastService } from 'src/app/core/services/toast.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit{
+  notifications:INotification [][];
   invesmentsCouts:IStatusWiseInvesment;
   clientsCounts:IStatusWiseClient;
   constructor(
-    private invesmentService: InvesmentService,private clientService: ClientService,
+    private invesmentService: InvesmentService,private clientService: ClientService,private notificationService: NotificationService,
     private toastService: ToastService
   ) {}
   ngOnInit(): void {
     this.countUsers();
     this.countInvesments();
+    this.allActiveNotifications();
   }
   countUsers(){
     this.clientService.getStatusWiseClients().subscribe((response)=>{
@@ -40,5 +44,23 @@ export class DashboardComponent implements OnInit{
         this.toastService.showError(response.message);
       }
     });
+  }
+  allActiveNotifications(){
+    this.notificationService.getActiveNotifications().subscribe((response)=>{
+      if(response.apiResponseStatus==1){
+        this.notifications = response.result;
+      }else{
+        this.toastService.showError(response.message);
+      }
+    });
+  }
+
+  getBackgroundColorClass(type: number): string {
+    
+    return type === 2 ? 'bg-blue-100' : 'bg-orange-100';
+  }
+
+  getIconClass(type: number): string {
+    return type === 2 ? 'pi pi-exclamation-circle text-xl text-blue-500' : 'pi pi-exclamation-triangle text-xl text-orange-500';
   }
 }
