ee7233810b7306cb27e07b68b9a5ca898c1bcfeb
somnathdhar97 <somnathdhar2021@gmail.com>
2023-12-07 01:48:56 +0530

    code organized..

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { NotfoundComponent } from './demo/components/notfound/notfound.component';
 import { AppLayoutComponent } from './core/layout/app.layout.component';
 import { authGuard } from './core/guards/auth.guard';
-// import { AppLayoutComponent } from "./layout/app.layout.component";
 
 @NgModule({
     imports: [
@@ -12,30 +11,16 @@ import { authGuard } from './core/guards/auth.guard';
                 path: '', component: AppLayoutComponent, canActivate: [authGuard], data: { role: ['client', 'admin'] },
                 children: [
                     { path: '', loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule) },
-                    { path: 'invest', loadChildren: () => import('./features/investment/investment.module').then(m => m.InvestmentModule) },
-                    { path: 'return', loadChildren: () => import('./features/return/return.module').then(m => m.ReturnModule) },
-                    { path: 'createuser', loadChildren: () => import('./core/auth/admin-registration/admin-registration.module').then(m => m.AdminRegistrationModule) },
-                    { path: 'client', loadChildren: () => import('./features/client/client.module').then(m => m.ClientModule) },
-                    { path: 'notification', loadChildren: () => import('./features/notification/notification.module').then(m => m.NotificationModule) },
+                    { path: 'invest', canActivate: [authGuard], data: { role: ['client', 'admin'] }, loadChildren: () => import('./features/investment/investment.module').then(m => m.InvestmentModule) },
+                    { path: 'return', canActivate: [authGuard], data: { role: ['admin'] }, loadChildren: () => import('./features/return/return.module').then(m => m.ReturnModule) },
+                    { path: 'createuser', canActivate: [authGuard], data: { role: ['admin'] }, loadChildren: () => import('./core/auth/admin-registration/admin-registration.module').then(m => m.AdminRegistrationModule) },
+                    { path: 'client', canActivate: [authGuard], data: { role: ['admin'] }, loadChildren: () => import('./features/client/client.module').then(m => m.ClientModule) },
+                    { path: 'notification', canActivate: [authGuard], data: { role: ['admin'] }, loadChildren: () => import('./features/notification/notification.module').then(m => m.NotificationModule) },
                 ]
             },
             { path: '', redirectTo: 'login', pathMatch: 'full' },
             { path: 'login', loadChildren: () => import('./core/auth/login/login.module').then(m => m.LoginModule) },
-            { path: 'register', loadChildren: () => import('./core/auth/register/register.module').then(m => m.RegisterModule) },
-            {
-                path: 'dashboard', component: AppLayoutComponent,
-                children: [
-
-                    // { path: '', loadChildren: () => import('./demo/components/dashboard/dashboard.module').then(m => m.DashboardModule) },
-                    // { path: 'uikit', loadChildren: () => import('./demo/components/uikit/uikit.module').then(m => m.UIkitModule) },
-                    // { path: 'utilities', loadChildren: () => import('./demo/components/utilities/utilities.module').then(m => m.UtilitiesModule) },
-                    // { path: 'documentation', loadChildren: () => import('./demo/components/documentation/documentation.module').then(m => m.DocumentationModule) },
-                    // { path: 'blocks', loadChildren: () => import('./demo/components/primeblocks/primeblocks.module').then(m => m.PrimeBlocksModule) },
-                    // { path: 'pages', loadChildren: () => import('./demo/components/pages/pages.module').then(m => m.PagesModule) }
-                ]
-            },
-            // { path: 'landing', loadChildren: () => import('./demo/components/landing/landing.module').then(m => m.LandingModule) },
-            { path: 'auth', loadChildren: () => import('./core/auth/auth.module').then(m => m.AuthModule) },
+            { path: 'registration', loadChildren: () => import('./core/auth/register/register.module').then(m => m.RegisterModule) },
             { path: 'notfound', component: NotfoundComponent },
             { path: '**', redirectTo: '/notfound' },
         ], { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' })
