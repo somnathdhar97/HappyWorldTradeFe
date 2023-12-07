5704fe42c11d28dd5f50da5d79b1d67d57994b71
somnathdhar97 <somnathdhar2021@gmail.com>
2023-12-08 03:55:18 +0530

    return payment is done..

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ import { authGuard } from './core/guards/auth.guard';
                 children: [
                     { path: '', loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule) },
                     { path: 'invest', canActivate: [authGuard], data: { role: ['client', 'admin'] }, loadChildren: () => import('./features/investment/investment.module').then(m => m.InvestmentModule) },
-                    { path: 'return', canActivate: [authGuard], data: { role: ['admin'] }, loadChildren: () => import('./features/return/return.module').then(m => m.ReturnModule) },
+                    { path: 'return/:investmentId', canActivate: [authGuard], data: { role: ['admin'] }, loadChildren: () => import('./features/return/return.module').then(m => m.ReturnModule) },
                     { path: 'createuser', canActivate: [authGuard], data: { role: ['admin'] }, loadChildren: () => import('./core/auth/admin-registration/admin-registration.module').then(m => m.AdminRegistrationModule) },
                     { path: 'client', canActivate: [authGuard], data: { role: ['admin'] }, loadChildren: () => import('./features/client/client.module').then(m => m.ClientModule) },
                     { path: 'notification', canActivate: [authGuard], data: { role: ['admin'] }, loadChildren: () => import('./features/notification/notification.module').then(m => m.NotificationModule) },
